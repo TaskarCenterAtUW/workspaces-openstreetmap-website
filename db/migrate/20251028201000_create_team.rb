5500a95325f3a126007ad5bdb5da73739bbc6b3c
uwjohnbee <johnbee@uw.edu>
2025-10-28 16:25:25 -0400

    adding migration

---
+++ db/migrate/20251028201000_create_team.rb
@@ -0,0 +1,24 @@
+class CreateTeam < ActiveRecord::Migration[7.1]
+  def self.up
+    create_table :teams, primary_key: :id do |t|
+      t.integer :id
+      t.string :name
+    end
+
+    add_index :teams, :name, unique: true
+
+    create_table :team_user do |t|
+      t.integer :team_id, null: false
+      t.integer :user_id, null: false
+    end
+
+    add_index :team_user, [:team_id, :user_id], unique: true
+    add_foreign_key :team_user, :teams, column: :team_id
+    add_foreign_key :team_user, :users, column: :user_id
+  end
+  
+  def self.down
+    drop_table :team_user
+    drop_table :teams
+  end
+end
