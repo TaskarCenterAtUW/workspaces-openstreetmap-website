d49a4ef5a5e6a32417f67de5b6c4e8a8ea5c6e6c
Cy Rossignol <cy@rossignols.me>
2025-11-06 22:00:29 -0800

    Fix migrations references for tenant schemas

--- db/migrate/20241030090336_add_user_location_name.rb
+++ db/migrate/20241030090336_add_user_location_name.rb
@@ -1,5 +1,9 @@
 class AddUserLocationName < ActiveRecord::Migration[7.2]
   def change
+    if connection.current_schema.start_with?("workspace-")
+      return # Ignore this migration in workspace tenant schemas
+    end
+
     add_column :users, :home_location_name, :string
   end
 end
