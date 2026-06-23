7caa95d2300953e074f5d211342339b31a77101a
Naresh Kumar D <susrisha@gmail.com>
2026-06-23 16:11:46 +0530

    Reverting extra changes
    Reverting extra changes that may not be required.

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -111,7 +111,7 @@ Apartment.configure do |config|
   #
   # config.persistent_schemas = %w{ hstore }
   # Ensure postgis is always in the search path, so that we can use PostGIS functions in our queries
-  config.persistent_schemas = %w{ public, shared_extensions }
+  # config.persistent_schemas = %w{ public, shared_extensions }
 
   # <== PostgreSQL only options
   #
